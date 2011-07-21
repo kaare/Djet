ee3f108f36c45ff10ad49a7e0f55e3d35a89b287
Kaare Rasmussen <kaare@jasonic.dk>
2011-07-21 16:07:20 +0200

    Try convert_blessed_universel. Turn methods into functions

--- lib/Jet/Role/Log.pm
+++ lib/Jet/Role/Log.pm
@@ -2,10 +2,9 @@ package Jet::Role::Log;
 
 use 5.010;
 use Moose::Role;
-use JSON;
+use JSON -convert_blessed_universally;
 
 sub trace {
-	my $self = shift;
 	my @package;
 	my $level = 0;
 	while (my ($package, $filename, $line, $sub, $hasargs, $wantarray, $evaltext, $is_require) = caller($level++)) {
@@ -13,13 +12,12 @@ sub trace {
 
 		push @package, "$level: $package, Sub: $sub, file: $filename, line: $line";
 	}
-	print STDERR JSON->new->allow_blessed->pretty->encode({stack => \@package, values => \@_} );
+	print STDERR JSON->new->allow_blessed->convert_blessed->pretty->encode({stack => \@package, values => \@_} );
 }
 
 sub debug {
-	my $self = shift;
 	my($package, $filename, $line) = caller;
-	print STDERR JSON->new->allow_blessed->pretty->encode({package => $package, file => $filename, line => $line, values => \@_ });
+	print STDERR JSON->new->allow_blessed->convert_blessed->pretty->encode({package => $package, file => $filename, line => $line, values => \@_ });
 }
 
 no Moose::Role;
