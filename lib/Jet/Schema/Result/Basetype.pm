b09abcfa102397fc15c65ed2bda128602406ef2d
Kaare Rasmussen <karm@novozymes.com>
2013-09-03 13:51:08 +0200

    Add dbicdump generated Jet schema.

---
+++ lib/Jet/Schema/Result/Basetype.pm
@@ -0,0 +1,169 @@
+use utf8;
+package Jet::Schema::Result::Basetype;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Jet::Schema::Result::Basetype - Node Base Type
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<basetype>
+
+=cut
+
+__PACKAGE__->table("basetype");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'jet.basetype_id_seq'
+
+=head2 name
+
+  data_type: 'text'
+  is_nullable: 1
+
+Base Name
+
+=head2 parent
+
+  data_type: 'integer[]'
+  is_nullable: 1
+
+Array of allowed parent basetypes
+
+=head2 datacolumns
+
+  data_type: 'json'
+  is_nullable: 1
+
+The column definitions
+
+=head2 searchable
+
+  data_type: 'text[]'
+  is_nullable: 1
+
+The searchable columns
+
+=head2 handler
+
+  data_type: 'text'
+  is_nullable: 1
+
+The handler module
+
+=head2 template
+
+  data_type: 'text'
+  is_nullable: 1
+
+The template for this basetype
+
+=head2 created
+
+  data_type: 'timestamp'
+  default_value: current_timestamp
+  is_nullable: 1
+  original: {default_value => \"now()"}
+
+=head2 modified
+
+  data_type: 'timestamp'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "jet.basetype_id_seq",
+  },
+  "name",
+  { data_type => "text", is_nullable => 1 },
+  "parent",
+  { data_type => "integer[]", is_nullable => 1 },
+  "datacolumns",
+  { data_type => "json", is_nullable => 1 },
+  "searchable",
+  { data_type => "text[]", is_nullable => 1 },
+  "handler",
+  { data_type => "text", is_nullable => 1 },
+  "template",
+  { data_type => "text", is_nullable => 1 },
+  "created",
+  {
+    data_type     => "timestamp",
+    default_value => \"current_timestamp",
+    is_nullable   => 1,
+    original      => { default_value => \"now()" },
+  },
+  "modified",
+  { data_type => "timestamp", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<basetype_name_key>
+
+=over 4
+
+=item * L</name>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("basetype_name_key", ["name"]);
+
+=head1 RELATIONS
+
+=head2 datas
+
+Type: has_many
+
+Related object: L<Jet::Schema::Result::Data>
+
+=cut
+
+__PACKAGE__->has_many(
+  "datas",
+  "Jet::Schema::Result::Data",
+  { "foreign.basetype_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07036 @ 2013-09-03 13:46:26
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:o1yNxRaCsZkP1vmvgqwC2w
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
---
+++ lib/Jet/Schema/Result/Node.pm
@@ -0,0 +1,191 @@
+use utf8;
+package Jet::Schema::Result::Node;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Jet::Schema::Result::Node - Node
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<node>
+
+=cut
+
+__PACKAGE__->table("node");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'jet.node_id_seq'
+
+=head2 data_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+=head2 parent_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 1
+
+Parent of this uri
+
+=head2 part
+
+  data_type: 'text'
+  is_nullable: 1
+
+Path part
+
+=head2 node_path
+
+  data_type: 'text'
+  is_nullable: 1
+
+Global Path parts
+
+=head2 created
+
+  data_type: 'timestamp'
+  default_value: current_timestamp
+  is_nullable: 1
+  original: {default_value => \"now()"}
+
+=head2 modified
+
+  data_type: 'timestamp'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "jet.node_id_seq",
+  },
+  "data_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "parent_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+  "part",
+  { data_type => "text", is_nullable => 1 },
+  "node_path",
+  { data_type => "text", is_nullable => 1 },
+  "created",
+  {
+    data_type     => "timestamp",
+    default_value => \"current_timestamp",
+    is_nullable   => 1,
+    original      => { default_value => \"now()" },
+  },
+  "modified",
+  { data_type => "timestamp", is_nullable => 1 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 UNIQUE CONSTRAINTS
+
+=head2 C<node_node_path_key>
+
+=over 4
+
+=item * L</node_path>
+
+=back
+
+=cut
+
+__PACKAGE__->add_unique_constraint("node_node_path_key", ["node_path"]);
+
+=head1 RELATIONS
+
+=head2 data
+
+Type: belongs_to
+
+Related object: L<Jet::Schema::Result::Data>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "data",
+  "Jet::Schema::Result::Data",
+  { id => "data_id" },
+  {
+    is_deferrable => 0,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+=head2 nodes
+
+Type: has_many
+
+Related object: L<Jet::Schema::Result::Node>
+
+=cut
+
+__PACKAGE__->has_many(
+  "nodes",
+  "Jet::Schema::Result::Node",
+  { "foreign.parent_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
+=head2 parent
+
+Type: belongs_to
+
+Related object: L<Jet::Schema::Result::Node>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "parent",
+  "Jet::Schema::Result::Node",
+  { id => "parent_id" },
+  {
+    is_deferrable => 0,
+    join_type     => "LEFT",
+    on_delete     => "CASCADE",
+    on_update     => "CASCADE",
+  },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07036 @ 2013-09-03 13:46:26
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:0PlpKcM2/ULq2f9dXtOAJQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
