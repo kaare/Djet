ce4e80df0fdbda8c4295bab1a01275768f767158
Kaare Rasmussen <karm@novozymes.com>
2013-10-29 13:29:06 +0100

    Remember the rest of the node path

--- lib/Jet/Schema/ResultSet/DataNode.pm
+++ lib/Jet/Schema/ResultSet/DataNode.pm
@@ -1,6 +1,27 @@
 package Jet::Schema::ResultSet::DataNode;
 use base 'DBIx::Class::ResultSet';
 
+use Moose;
+use namespace::autoclean;
+
+=head1 ATTRIBUTES
+
+=head2 rest_path
+
+The remaining part after the basenode has been found.
+
+=cut
+
+has rest_path => (
+    is => 'ro',
+    isa => 'Str',
+    writer => 'set_rest_path',
+);
+
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
+
+=head1 METHODS
+
 =head2 find_basenode
 
 Find the basenode
@@ -10,12 +31,20 @@ Returns a set of rows as an arrayref, starting from the basenode and with the ro
 Thus, we're sure always to have to whole branch, and we can also find the
 arguments of the request
 
+As a side effect this method sets rest_path.
+
 =cut
 
 sub find_basenode {
 	my ($self, $path) = @_;
-	$path =~ s|^(.*?)/?$|$1|; # Remove last character if slash
-	return $self->search({node_path => { '@>' => $path } }, {order_by => \'length(node_path) DESC' });
+	my $datanodes = $self->search({node_path => { '@>' => $path } }, {order_by => \'length(node_path) DESC' });
+    my $basenode = $datanodes->first or return;
+
+    my $base_path = $basenode->node_path;
+    my $rest = $path;
+    $rest =~ s|^$base_path/*||;
+    $self->set_rest_path($rest);
+	return $datanodes;
 }
 
 1;
