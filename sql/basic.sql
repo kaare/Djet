8632da299e1ab156f7086c4fe2e3e0c94f22a44f
Kaare Rasmussen <kaare@jasonic.dk>
2014-02-04 04:04:30 +0100

    Init data update

--- sql/basic.sql
+++ sql/basic.sql
@@ -7,8 +7,8 @@ SET search_path=jet, public;
 -- Basetypes
 
 INSERT INTO basetype (name,title) VALUES ('directory','Directory');
-INSERT INTO basetype (name,title) VALUES ('jet_config', 'Jet Configuration');
-INSERT INTO basetype (name,title,handler,datacolumns) VALUES ('jet_basetype','Jet Basetype','Jet::Engine::Basetype','[{"name":"name","type":"Str", "traits": ["Jet::Trait::Config::Basetype"]},{"name":"parent","type":"Int"}]');
+INSERT INTO basetype (name,title,handler) VALUES ('jet_config', 'Jet Configuration','Jet::Engine::Config');
+INSERT INTO basetype (name,title,handler,datacolumns) VALUES ('jet_basetype','Jet Basetype','Jet::Engine::Basetype','[{"name":"text","type":"Str", "traits": ["Jet::Trait::Config::Basetype"]},{"name":"parent","type":"Int"}]');
 INSERT INTO basetype (name,title) VALUES ('not_found', 'Not Found');
 -- INSERT INTO basetype (name,title) VALUES ('usergroup', 'User Group');
 -- INSERT INTO basetype (name,parent,datacolumns,searchable) VALUES ('person','{2,3}', '[{"name":"userlogin","type":"Str"},{"name":"password","type":"Password"}]', '{"userlogin"}');
@@ -18,8 +18,8 @@ INSERT INTO basetype (name,title) VALUES ('not_found', 'Not Found');
 
 INSERT INTO data_node (basetype_id,part,name,title) VALUES (1,'','Root','Root Directory');
 INSERT INTO data_node (basetype_id,parent_id,part,name,title) VALUES (1,1,'jet','Jet Base Directory','Jet Base Directory');
-INSERT INTO data_node (basetype_id,parent_id,part,name,title) VALUES (2, 2,'config','Jet Configuration', 'Jet Configuration');
-INSERT INTO data_node (basetype_id,parent_id,part,name,title) VALUES (3, 3,'basetype','Jet Configuration - Basetypes', 'Jet Configuration - Basetypes');
+INSERT INTO data_node (basetype_id,parent_id,part,name,title) VALUES (2, 1,'config','Jet Configuration', 'Jet Configuration');
+INSERT INTO data_node (basetype_id,parent_id,part,name,title) VALUES (3, 1,'basetype','Jet Configuration - Basetypes', 'Jet Configuration - Basetypes');
 INSERT INTO data_node (basetype_id,part,name,title) VALUES (4,'not_found','not_found','Not Found');
 
 -- INSERT INTO data_node (basetype_id,part,name,datacolumns) VALUES (6,'read','read','{read}');
