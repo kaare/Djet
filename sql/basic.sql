1028009a9d456dc30a1200207cd8dc8a6055a055
Kaare Rasmussen <kaare@jasonic.dk>
2014-05-27 21:19:22 +0200

    Add feature to the sql.

--- sql/basic.sql
+++ sql/basic.sql
@@ -4,12 +4,16 @@ BEGIN;
 
 SET search_path=jet, public;
 
+-- Features
+
+INSERT INTO feature (name,version, description) VALUES ('basic', 0.01, 'Basic features');
+
 -- Basetypes
 
-INSERT INTO basetype (name,title) VALUES ('directory','Directory');
-INSERT INTO basetype (name,title,handler) VALUES ('jet_config', 'Jet Configuration','Jet::Engine::Config');
+INSERT INTO basetype (feature_id, name,title) VALUES (1, 'directory','Directory');
+INSERT INTO basetype (feature_id, name,title,handler) VALUES (1, 'jet_config', 'Jet Configuration','Jet::Engine::Config');
 -- INSERT INTO basetype (name,title,handler,datacolumns) VALUES ('jet_basetype','Jet Basetype','Jet::Engine::Basetype','[{"name":"text","type":"Str", "traits": ["Jet::Trait::Config::Basetype"]},{"name":"parent","type":"Int"}]');
-INSERT INTO basetype (name,title,handler,datacolumns) VALUES ('jet_basetype','Jet Basetype','Jet::Engine::Basetype','[{"name":"text","type":"Str"},{"name":"parent","type":"Int"}]');
+INSERT INTO basetype (feature_id, name,title,handler,datacolumns) VALUES (1, 'jet_basetype','Jet Basetype','Jet::Engine::Basetype','[{"name":"text","type":"Str"},{"name":"parent","type":"Int"}]');
 -- INSERT INTO basetype (name,title) VALUES ('not_found', 'Not Found');
 -- INSERT INTO basetype (name,title) VALUES ('usergroup', 'User Group');
 -- INSERT INTO basetype (name,parent,datacolumns,searchable) VALUES ('person','{2,3}', '[{"name":"userlogin","type":"Str"},{"name":"password","type":"Password"}]', '{"userlogin"}');
