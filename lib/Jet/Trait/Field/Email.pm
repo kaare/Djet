f6c8338cdb51d8236f19c97498c0de5cb1a4a668
Kaare Rasmussen <kaare@jasonic.dk>
2014-08-17 21:07:27 +0200

    Add new Email field type. Nothing in it yet, really, but it's necessary for later validation method

---
+++ lib/Jet/Trait/Field/Email.pm
@@ -0,0 +1,19 @@
+package Jet::Trait::Field::Email;
+
+use Moose::Role;
+
+=head1 NAME
+
+Jet::Trait::Field::Email - decorate the email field
+
+=cut
+
+requires qw/value/;
+
+=head1 METHODS
+
+Nothing yet
+
+=cut
+
+1;
