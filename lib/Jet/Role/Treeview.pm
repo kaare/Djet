596c10e84c28e0cb304d5d64adbeeb5a3242f17d
Kaare Rasmussen <kaare@jasonic.dk>
2014-09-27 09:06:04 +0200

    Urify the paths

--- lib/Jet/Role/Treeview.pm
+++ lib/Jet/Role/Treeview.pm
@@ -28,16 +28,17 @@ before to_json => sub {
 	my $self = shift;
 	if (my ($template) = $self->request->parameters->{template} =~ /^tree(top|view)$/) {
 		my $basenode = $self->basenode;
+		my $domain_basetype = $self->schema->basetype_by_name('domain');
+		my $domain_node = $self->datanode_by_basetype($domain_basetype);
 		my $dynadata;
 		if ($template eq 'top') {
-			my $domain_basetype = $self->schema->basetype_by_name('domain');
-			my $domain_node = $self->datanode_by_basetype($domain_basetype);
 			my $folder = $domain_node->has_children ? 1 : undef;
+			my $path = $domain_node->urify($domain_node)
 			$dynadata = [ {
 				title => $domain_node->title,
 				folder => $folder,
 				lazy => $folder,
-				path => $domain_node->node_path,
+				path => $path,
 				id   => $domain_node->node_id,
 			} ];
 		} else { # treeview
@@ -49,9 +50,10 @@ before to_json => sub {
 			}
 			$dynadata = [ map {
 				my $folder = $_->has_children ? 1 : undef;
+				my $path = $_->urify($domain_node)
 				{
 					title => $_->part,
-					path => $_->node_path,
+					path => $path,
 					id   => $_->node_id,
 					folder => $folder,
 					lazy => $folder,
