10b2ef8a58fdbd02c4815943eb1d3d2dd8c13fe7
Kaare Rasmussen <kaare@jasonic.dk>
2014-09-28 14:37:43 +0200

    Add 'News' feature to Jet

---
+++ lib/Jet/Engine/News.pm
@@ -0,0 +1,57 @@
+package Jet::Engine::News;
+
+use 5.010;
+use Moose;
+
+extends 'Jet::Engine::Children';
+
+=head1 NAME
+
+Jet::Engine::News
+
+=head2 DESCRIPTION
+
+News items are grouped under a News node. Displayed sorted descending on date with
+a configurable limit. Older news items are archived under their separate years. 
+
+=head1 METHODS
+
+=head2 before init_data
+
+Set the list name to 'news_feed'
+
+=cut
+
+before 'init_data' => sub  {
+	my $self = shift;
+	$self->set_list_name('news_feed');
+};
+
+=head2 after data
+
+Set the list name to 'news_feed'
+
+=cut
+
+after 'data' => sub  {
+	my $self = shift;
+	my $stash = $self->stash;
+	if (my $year = $self->request->param('year')) {
+warn $year;
+		$self->add_search(data_created => { '-between' => [ $year . '-01-01', $year . '-12-31' ] });
+	}
+	my $archive = $stash->{news_feed};
+	$stash->{news_archive} = $archive->search(undef, {
+		select => [ \"date_part('year', data_created)", \"count(*)" ],
+		as => [qw/year count/],
+		group_by => 1,
+		order_by => { '-desc' => 1},
+	});
+};
+
+
+__PACKAGE__->meta->make_immutable;
+
+# COPYRIGHT
+
+__END__
---
+++ sql/news.sql
@@ -0,0 +1,24 @@
+-- Jet basetypes and nodes for the News Feature
+
+BEGIN;
+
+SET search_path=jet, public;
+
+-- Features
+
+INSERT INTO feature (name,version, description) VALUES ('news', 0.01, 'News features');
+
+-- Basetypes
+
+INSERT INTO basetype (feature_id,name,title,datacolumns,handler) VALUES (currval('feature_id_seq'), 'news','News','[
+	{"type":"Boolean","title":"Menu","name":"topmenu"}
+]','Jet::Engine::News');
+INSERT INTO basetype (feature_id,name,title,datacolumns,template) VALUES (currval('feature_id_seq'), 'news-item','News Item','[
+	{"name":"news_text","title":"Text","type":"Html"}
+]','<domain>/basetype/news_item.tx');
+
+-- Data Nodes
+
+-- INSERT INTO data_node (basetype_id,parent_id,part,name,title,datacolumns) VALUES (currval('basetype_id_seq'),1,'news','news','News','{"topmenu":"on"}');
+
+COMMIT;
