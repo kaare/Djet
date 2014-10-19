1ace9c759f632e5713973ce6ea75092a98bc2b61
Kaare Rasmussen <kaare@jasonic.dk>
2014-10-19 08:14:26 +0200

    Show the minicart at startup

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,9 +2,11 @@ $(function() {
 	// there's the gallery and the trash
 	$(".add-to-basket").click(function() {
 		$.post(
-			'/cart',
+			$("#cart_url").text(),
 			$(this).data(),
 			function (data) {
+				// Show minicart
+				$('#minicart').removeClass('hidden');
 				// Update minicart
 				var minicart = jQuery.parseJSON( data );
 				$('#minicart-quantity').text(minicart.quantity);
