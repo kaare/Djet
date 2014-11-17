f162923abced30f6676312b3d39632278191ef87
Kaare Rasmussen <kaare@jasonic.dk>
2014-11-17 20:27:25 +0100

    Use the new unpack structure, and also create a method to display the structured data for editing.

--- lib/Jet/Trait/Field/Structured.pm
+++ lib/Jet/Trait/Field/Structured.pm
@@ -15,6 +15,21 @@ Structured data. Input format is expected as pseudo yaml (yaml w/o the initial -
 
 requires qw/value/;
 
+=head1 ATTRIBUTES
+
+=head2 validated_value
+
+Value from the validation
+
+=cut
+
+has 'validated_value' => (
+	is => 'ro',
+#	isa => 'HashRef',
+	writer => '_set_validated_value',
+	predicate => '_has_validated_value',
+);
+
 =head1 METHODS
 
 =head2 pack
@@ -28,6 +43,61 @@ sub pack {
 	my $new;
 	eval {$new = YAML::Tiny::Load("---\n" . $value) };
 	return $@ ? $value : $new;
+}
+
+=head2 unpack
+
+Unpack a structured field
+
+=cut
+
+sub unpack {
+	my ($self, $value) = @_;
+warn 1;
+	$value //= $self->value;
+	return $self->_has_validated_value ? $self->validated_value : $value;
+}
+
+=head2 constraint_methods
+
+=cut
+
+sub constraint_methods {
+	my $self = shift;
+	return $self->name => sub {
+		my $dfv = shift;
+
+        # value of 'prospective_date' parameter
+        my $value = $dfv->get_current_constraint_value();
+
+        # get other data to refer to
+        my $data = $dfv->get_filtered_data;
+
+		my $new;
+		eval {$new = YAML::Tiny::Load("---\n" . $value) };
+		my $error = $@;
+		$self->_set_validated_value($new) unless $error;
+		return !$error;
+	};
+}
+
+=head2 value_for_editing
+
+Return a structured field for editing
+
+=cut
+
+sub value_for_editing {
+	my ($self, $value) = @_;
+	$value //= $self->value;
+	my $new;
+	eval {$new = YAML::Tiny::Dump($value) };
+	if ($@) {
+		return $value;
+	} else {
+		$new =~ s/^---\n//m;
+		return $new;
+	}
 }
 
 1;
