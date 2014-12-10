163c1b38994c3581d7d58bc9bf2df46ceb28fcf7
Kaare Rasmussen <kaare@jasonic.dk>
2014-12-10 12:27:17 +0100

    Actually commit the logout engine

---
+++ lib/Djet/Engine/Logout.pm
@@ -0,0 +1,36 @@
+package Djet::Engine::Logout;
+
+use 5.010;
+use Moose;
+
+extends 'Djet::Engine::Default';
+
+=head1 NAME
+
+Djet::Engine::Logout
+
+=head2 DESCRIPTION
+
+Removes the user from the session
+
+=head1 METHODS
+
+=head2 before data
+
+Remove the user's session, and redirect to whatever was set as redirect_uri, or to /
+
+=cut
+
+before 'data' => sub  {
+	my $self = shift;
+	my $redirect_uri = delete $self->session->{redirect_uri} // '/';
+	delete $self->session->{djet_user};
+	$self->response->header('Location' => $redirect_uri);
+	$self->return_value(\302);
+};
+
+__PACKAGE__->meta->make_immutable;
+
+# COPYRIGHT
+
+__END__
