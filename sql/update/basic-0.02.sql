da74090a60f863ba5dfdfdaf97d008f529cc582f
Kaare Rasmussen <kaare@jasonic.dk>
2015-12-13 09:24:51 +0100

    The directory type, sql

--- sql/update/basic-0.02.sql
+++ sql/update/basic-0.02.sql
@@ -10,5 +10,14 @@ UPDATE feature SET version = 0.02 WHERE name='basic';
 
 -- Basetypes
 
-INSERT INTO basetype (feature_id, name,title,datacolumns) VALUES (1, 'directory','Directory','[{"name":"topmenu","title":"Topmenu","type":"Boolean"}]');
-UPDATE basetype SET datacolumns='[ {"name":"topmenu","title":"Topmenu","type":"Boolean"}, {"name":"children","title":"Children","type":"Str"}, {"name":"order","title":"Sort Order","type":"Str"}, {"name":"search","title":"Search Options","type":"Str"}, {"name":"pagination","title":"Pagination","type":"Str"}, {"name":"listname","title":"List Name","type":"Str"}, {"name":"listname","title":"List Name","type":"Str"} ]' WHERE feature_id = 1 AND name = 'directory';
+UPDATE basetype SET
+	datacolumns = '[
+		{"name":"topmenu","title":"Topmenu","type":"Boolean"},
+		{"name":"children","title":"Children","type":"Str"},
+		{"name":"order","title":"Sort Order","type":"Str"},
+		{"name":"search","title":"Search Options","type":"Str"},
+		{"name":"pagination","title":"Pagination","type":"Str"},
+		{"name":"listname","title":"List Name","type":"Str"}
+	]',
+	handler = 'Djet::Engine::Directory'
+	WHERE feature_id = 1 AND name = 'directory';
