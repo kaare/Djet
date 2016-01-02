6af24c65d94661bd2a63772e8f66cd9fed397473
Kaare Rasmussen <kaare@jasonic.dk>
2016-01-02 20:22:36 +0100

    Code style

--- sql/recipe.sql
+++ sql/recipe.sql
@@ -10,19 +10,8 @@ INSERT INTO feature (name,version, description) VALUES ('recipe', 0.01, 'Recipe 
 
 -- Basetypes
 
-INSERT INTO basetype (feature_id,name,title,datacolumns,template) VALUES (currval('feature_id_seq'), 'recipe','Recipe','[
-	{"name":"ingredients","title":"Ingredients","type":"Html", "required": "on", "searchable":"on"},
-	{"name":"procedure","title":"Procedure","type":"Html", "required": "on", "searchable":"on"},
-	{"name":"image","title":"Image Path","type":"File"},
-	{"name":"persons","title":"Persons","type":"Int", "searchable":"on"},
-	{"name":"category","title":"Category","type":"Str", "searchable":"on"},
-	{"name":"main_indredient","title":"Main Ingredient","type":"Str", "searchable":"on"},
-	{"name":"occasion","title":"Occasion","type":"Str", "searchable":"on"},
-	{"name":"remarks","title":"Remarks","type":"Str", "searchable":"on"},
-	{"name":"publish_date","title":"Publish Date","type":"Date", "required": "on", "searchable":"on"},
-	{"name":"tags","title":"Tags","type":"Str", "searchable":"on"},
-	{"name":"keywords","title":"Keywords","type":"Str", "searchable":"on"}
-]','<domain>/basetype/recipe.tx');
+INSERT INTO basetype (feature_id,name,title,datacolumns,template) VALUES (currval('feature_id_seq'), 'recipe','Recipe','[ {"name":"ingredients","title":"Ingredients","type":"Html", "required": "on", "searchable":"on"}, {"name":"procedure","title":"Procedure","type":"Html", "required": "on", "searchable":"on"}, {"name":"image","title":"Image Path","type":"File"}, {"name":"persons","title":"Persons","type":"Int", "searchable":"on"}, {"name":"category","title":"Category","type":"Str", "searchable":"on"}, {"name":"main_indredient","title":"Main Ingredient","type":"Str", "searchable":"on"}, {"name":"occasion","title":"Occasion","type":"Str", "searchable":"on"}, {"name":"remarks","title":"Remarks","type":"Str", "searchable":"on"}, {"name":"publish_date","title":"Publish Date","type":"Date", "required": "on", "searchable":"on"}, {"name":"tags","title":"Tags","type":"Str", "searchable":"on"}, {"name":"keywords","title":"Keywords","type":"Str", "searchable":"on"} ]'
+,'<domain>/basetype/recipe.tx');
 
 
 COMMIT;
