78b32c948594b2e02a88435475764e877ab9ea62
Kaare Rasmussen <kaare@jasonic.dk>
2018-08-01 05:00:40 +0000

    Silence warnings

--- lib/Djet/Trait/Field/Price.pm
+++ lib/Djet/Trait/Field/Price.pm
@@ -20,24 +20,25 @@ Return a nicely formatted value
 =cut
 
 sub formatted_value {
-	my $self = shift;
-	my %params = @_;
-	my $language = $params{language} || $self->model->config->config->{language_price};
-	my $old_locale = setlocale(LC_MONETARY);
-	setlocale(LC_MONETARY, $language);
+    my $self = shift;
+    my %params = @_;
+    my $language = $params{language} || $self->model->config->config->{language_price};
+    my $old_locale = setlocale(LC_MONETARY);
+    setlocale(LC_MONETARY, $language);
 
-	my $locale = localeconv();
-	my $currency = $params{no_currency} ? '' : $locale->{int_curr_symbol};
-	my $decpt = $locale->{mon_decimal_point};
-	my $decdigits = $locale->{int_frac_digits};
-	my $value = $self->value;
-	if ($value =~ m/(\d+)[,.]?(\d*)/) {
-		my $int = $1;
-		my $frac = substr(sprintf("%.$decdigits" . 'f', ".$2"), 2, $decdigits) || '-';
-		$value = "$currency$int$decpt$frac";
-	}
-	setlocale(LC_MONETARY, $old_locale);
-	return $value;
+    my $locale = localeconv();
+    my $currency = $params{no_currency} ? '' : $locale->{int_curr_symbol};
+    my $decpt = $locale->{mon_decimal_point};
+    my $decdigits = $locale->{int_frac_digits};
+    my $value = $self->value;
+    if ($value =~ m/(\d+)[,.]?(\d*)/) {
+        my $frac = $2 || 0;
+        my $int = $1;
+        $frac = substr(sprintf("%.$decdigits" . 'f', ".$frac"), 2, $decdigits) || '-';
+        $value = "$currency$int$decpt$frac";
+    }
+    setlocale(LC_MONETARY, $old_locale);
+    return $value;
 }
 
 no Moose::Role;
